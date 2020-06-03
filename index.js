d047c8763d17d74467132d8bb03ffafeb3603152
Kevin St.Onge <kevinstonge@gmail.com>
2020-06-03 18:28:07 -0400

    finished Car section including all stretch tasks

--- index.js
+++ index.js
@@ -78,12 +78,13 @@ Car.prototype.fill = function(gallonsOfFuel) {
   this.tank += gallonsOfFuel;
 }
 Car.prototype.drive = function(distance) {
-  this.odometer += distance;
-  this.tank -= (distance * this.milesPerGallon);
-  if (this.tank < 0) {
+  let overage = (this.tank * this.milesPerGallon) - distance;
+  if (overage < 0) {
+    this.odometer += this.tank * this.milesPerGallon;
     this.tank = 0;
     return `I ran out of fuel at ${this.odometer} miles`;
   }
+  else { this.odometer += distance; this.tank -= distance/this.milesPerGallon }
 }
 
 /*
