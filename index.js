ac28c555dda20b618ae057c9235ae83b4ef5bc76
Kevin St.Onge <kevinstonge@gmail.com>
2020-06-03 15:57:10 -0400

    finished up to Car.prototype.drive method

--- index.js
+++ index.js
@@ -39,8 +39,18 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(food) {
+  if (this.stomach.length==10) { return; }
+  this.stomach.push(food);
+}
+Person.prototype.poop = function() { this.stomach = []; }
+Person.prototype.toString = function() { 
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,8 +67,23 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallonsOfFuel) {
+  this.tank += gallonsOfFuel;
+}
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  this.tank -= (distance * this.milesPerGallon);
+  if (this.tank < 0) {
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`;
+  }
 }
 
 /*
